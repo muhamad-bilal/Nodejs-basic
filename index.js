494d7439331a48220e227698765f459173553d87
Muhammad Bilal <aloo@Xubuntu.myguest.virtualbox.org>
2024-08-03 21:33:19 +0500

    finished

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    } else if (filePath == './About') {
+        filePath = './About.html';
+    } else if (filePath == './contact') {
+        filePath = './contact.html';
+    }
+
+
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', (error, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${error.code} ..\n`);
+                res.end();
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(8080, () => {
+    console.log('Server running at http://localhost:8080/');
+});
